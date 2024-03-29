815365e03ab509071bdce3a17d3952e15b1d709a
Wilson E. Husin <whusin@firehydrant.com>
2024-03-29 14:49:09 -0700

    direnv in devcontainer

--- .devcontainer/post-attach.sh
+++ .devcontainer/post-attach.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
+# Install Go tools
 ./deps.sh
+
+# Install direnv hook
+grep -qxF 'include "direnv"' /home/vscode/.bashrc || echo 'eval "$(direnv hook bash)"' >> /home/vscode/.bashrc
