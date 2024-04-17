d90fdb8467a40c59a4e6fea17a2cdc23846cfddd
Alexis (AJ) Jasso <ajasso@firehydrant.io>
2024-04-17 14:42:24 -0700

    Add rotations as schedules

--- pager/opsgenie.go
+++ pager/opsgenie.go
@@ -2,17 +2,25 @@ package pager
 
 import (
 	"context"
+	"fmt"
+	"strconv"
+	"strings"
+	"time"
 
 	"github.com/firehydrant/signals-migrator/console"
+	"github.com/firehydrant/signals-migrator/store"
 	"github.com/gosimple/slug"
 	"github.com/opsgenie/opsgenie-go-sdk-v2/client"
+	"github.com/opsgenie/opsgenie-go-sdk-v2/og"
+	"github.com/opsgenie/opsgenie-go-sdk-v2/schedule"
 	"github.com/opsgenie/opsgenie-go-sdk-v2/team"
 	"github.com/opsgenie/opsgenie-go-sdk-v2/user"
 )
 
 type Opsgenie struct {
-	userClient *user.Client
-	teamClient *team.Client
+	userClient     *user.Client
+	teamClient     *team.Client
+	scheduleClient *schedule.Client
 }
 
 func NewOpsgenie(apiKey string) *Opsgenie {
@@ -26,9 +34,14 @@ func NewOpsgenie(apiKey string) *Opsgenie {
 		ApiKey: apiKey,
 	})
 
+	var scheduleClient, _ = schedule.NewClient(&client.Config{
+		ApiKey: apiKey,
+	})
+
 	return &Opsgenie{
-		userClient: userClient,
-		teamClient: teamClient,
+		userClient:     userClient,
+		teamClient:     teamClient,
+		scheduleClient: scheduleClient,
 	}
 }
 
@@ -37,8 +50,153 @@ func (p *Opsgenie) Kind() string {
 }
 
 func (o *Opsgenie) LoadSchedules(ctx context.Context) error {
-	// TODO: implement
-	console.Warnf("opsgenie.LoadSchedules is not currently supported.")
+	resp, err := o.scheduleClient.List(ctx, &schedule.ListRequest{
+		// this is the cleanest way I found of making a *bool.  Happy to replace it with anything more readable.
+		Expand: func() *bool { b := true; return &b }(),
+	})
+
+	if err != nil {
+		return err
+	}
+
+	for _, schedule := range resp.Schedule {
+		// To decide: check enabled field and don't create if false?
+		if err := o.saveScheduleToDB(ctx, schedule); err != nil {
+			return fmt.Errorf("error saving schedule to db: %w", err)
+		}
+	}
+
+	return nil
+}
+
+func (o *Opsgenie) saveScheduleToDB(ctx context.Context, s schedule.Schedule) error {
+	resp, err := o.scheduleClient.Get(ctx, &schedule.GetRequest{
+		IdentifierType:  schedule.Id,
+		IdentifierValue: s.Id,
+	})
+
+	if err != nil {
+		return err
+	}
+
+	// each Opsgenie schedule can have multiple rotations, where each rotation has participants, start and handoff date/times, and restrictions.
+	// as such, I think an opsgenie rotation best maps to a FH schedule, so let's try that.
+
+	for _, rotation := range resp.Schedule.Rotations {
+		if err := o.saveRotationToDB(ctx, s, rotation); err != nil {
+			return fmt.Errorf("error saving schedule to db: %w", err)
+		}
+	}
+	return nil
+}
+
+func (o *Opsgenie) saveRotationToDB(ctx context.Context, s schedule.Schedule, r og.Rotation) error {
+	// ExtSchedule
+	desc := fmt.Sprintf("%s (%s)", s.Description, r.Name)
+	desc = strings.TrimSpace(desc)
+
+	ogsHandoffTime := r.StartDate.Format(time.TimeOnly)
+	ogsHandoffDay := strings.ToLower(r.StartDate.Weekday().String())
+
+	var ogsStrategy string
+	switch r.Type {
+	case og.Daily:
+		ogsStrategy = "daily"
+	case og.Weekly:
+		ogsStrategy = "weekly"
+	default:
+		ogsStrategy = "custom"
+	}
+
+	ogsParams := store.InsertExtScheduleParams{
+		ID:            s.Id + "-" + r.Id,
+		Name:          s.Name + " - " + r.Name,
+		Timezone:      s.Timezone,
+		Description:   desc,
+		HandoffTime:   ogsHandoffTime,
+		HandoffDay:    ogsHandoffDay,
+		Strategy:      ogsStrategy,
+		ShiftDuration: "",
+	}
+
+	q := store.UseQueries(ctx)
+	if err := q.InsertExtSchedule(ctx, ogsParams); err != nil {
+		return fmt.Errorf("error saving schedule: %w", err)
+	}
+
+	// ExtScheduleTeam
+	if err := q.InsertExtScheduleTeam(ctx, store.InsertExtScheduleTeamParams{
+		ScheduleID: ogsParams.ID,
+		TeamID:     s.OwnerTeam.Id,
+	}); err != nil {
+		if strings.Contains(err.Error(), "FOREIGN KEY constraint") {
+			console.Warnf("Team %s not found for schedule %s, skipping...\n", s.OwnerTeam.Id, ogsParams.ID)
+		} else {
+			return fmt.Errorf("error saving schedule team: %w", err)
+		}
+	}
+
+	// ExtScheduleMembers
+	for _, p := range r.Participants {
+		if err := q.InsertExtScheduleMember(ctx, store.InsertExtScheduleMemberParams{
+			ScheduleID: ogsParams.ID,
+			UserID:     p.Id,
+		}); err != nil {
+			if strings.Contains(err.Error(), "FOREIGN KEY constraint") {
+				console.Warnf("User %s not found for schedule %s, skipping...\n", p.Id, ogsParams.ID)
+			} else if strings.Contains(err.Error(), "UNIQUE constraint") {
+				console.Warnf("User %s already exists for schedule %s, skipping duplicate...\n", p.Id, ogsParams.ID)
+			} else {
+				return fmt.Errorf("error saving schedule user: %w", err)
+			}
+		}
+	}
+
+	// ExtScheduleRestriction
+	if r.TimeRestriction != nil {
+		switch r.TimeRestriction.Type {
+		case og.WeekdayAndTimeOfDay:
+			for i, tr := range r.TimeRestriction.RestrictionList {
+				startTime, _ := time.Parse(time.TimeOnly, fmt.Sprintf("%d:%d", *tr.StartHour, *tr.StartMin))
+				endTime, _ := time.Parse(time.TimeOnly, fmt.Sprintf("%d:%d", *tr.EndHour, *tr.EndMin))
+
+				ogsRestrictionsParams := store.InsertExtScheduleRestrictionParams{
+					ScheduleID:       ogsParams.ID,
+					RestrictionIndex: strconv.Itoa(i),
+					StartDay:         strings.ToLower(string(tr.StartDay)),
+					StartTime:        startTime.Format(time.TimeOnly),
+					EndDay:           strings.ToLower(string(tr.EndDay)),
+					EndTime:          endTime.Format(time.TimeOnly),
+				}
+				if err := q.InsertExtScheduleRestriction(ctx, ogsRestrictionsParams); err != nil {
+					return fmt.Errorf("error saving time of day restriction: %w", err)
+				}
+			}
+		case og.TimeOfDay:
+			for i, tr := range r.TimeRestriction.RestrictionList {
+				for j := range 7 {
+					dayStr := strings.ToLower(time.Weekday(j).String())
+					startTime, _ := time.Parse(time.TimeOnly, fmt.Sprintf("%d:%d", *tr.StartHour, *tr.StartMin))
+					endTime, _ := time.Parse(time.TimeOnly, fmt.Sprintf("%d:%d", *tr.EndHour, *tr.EndMin))
+
+					ogsRestrictionsParams := store.InsertExtScheduleRestrictionParams{
+						ScheduleID:       ogsParams.ID,
+						RestrictionIndex: fmt.Sprintf("%d-%d", i, j),
+						StartDay:         dayStr,
+						StartTime:        startTime.Format(time.TimeOnly),
+						EndDay:           dayStr,
+						EndTime:          endTime.Format(time.TimeOnly),
+					}
+					if err := q.InsertExtScheduleRestriction(ctx, ogsRestrictionsParams); err != nil {
+						return fmt.Errorf("error saving time of day restriction: %w", err)
+					}
+				}
+			}
+		default:
+			console.Warnf("Unknown schedule restriction type '%s' for schedule '%s', skipping...\n", r.TimeRestriction.Type, ogsParams.ID)
+		}
+	}
+
 	return nil
 }
 
