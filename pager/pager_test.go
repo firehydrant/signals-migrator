51ce964629fb7e334cad5a49ce7bc03bd19a644d
Wilson E. Husin <whusin@firehydrant.com>
2024-04-18 21:12:58 -0700

    pager: templetize golden tests

--- pager/pager_test.go
+++ pager/pager_test.go
@@ -2,12 +2,21 @@ package pager_test
 
 import (
 	"context"
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"os"
+	"path/filepath"
 	"testing"
 
 	"github.com/firehydrant/signals-migrator/store"
+	"github.com/gosimple/slug"
+	"gotest.tools/golden"
 )
 
 func withTestDB(t *testing.T) context.Context {
+	t.Helper()
 	ctx := store.WithContext(context.Background())
 	t.Cleanup(func() {
 		if err := store.FromContext(ctx).Close(); err != nil {
@@ -15,4 +24,47 @@ func withTestDB(t *testing.T) context.Context {
 		}
 	})
 	return ctx
+}
+
+func pagerProviderHttpServer(t *testing.T) *httptest.Server {
+	t.Helper()
+	h := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		baseTestDir := t.Name()
+		// Get the root-most test name as directory.
+		// This should be "Test[ProviderName]", e.g. TestPagerDuty.
+		for b := filepath.Dir(baseTestDir); b != "."; b = filepath.Dir(b) {
+			baseTestDir = b
+		}
+		filename, err := url.JoinPath("testdata", baseTestDir, "apiserver", slug.Make(r.URL.Path)+".json")
+		if err != nil {
+			t.Fatalf("error joining path for expected response: %s", err)
+		}
+		if _, err := os.Stat(filename); err != nil {
+			t.Logf("file not found: %s", filename)
+			http.NotFound(w, r)
+			return
+		}
+		http.ServeFile(w, r, filename)
+	})
+	s := httptest.NewServer(h)
+	t.Cleanup(s.Close)
+	return s
+}
+
+func assertJSON(t *testing.T, got any) {
+	t.Helper()
+
+	b, err := json.MarshalIndent(got, "", "  ")
+	if err != nil {
+		t.Fatalf("error marshalling to json: %s", err)
+	}
+
+	// Ensure the file ends with a newline, so when editors aggresively
+	// auto-fix this, golden tests don't actually fail.
+	if b[len(b)-1] != '\n' {
+		b = append(b, '\n')
+	}
+
+	goldenFile := t.Name() + ".golden.json"
+	golden.Assert(t, string(b), goldenFile)
 }
--- pager/pagerduty_test.go
+++ pager/pagerduty_test.go
@@ -2,68 +2,31 @@ package pager_test
 
 import (
 	"context"
-	"net/http"
-	"net/http/httptest"
-	"net/url"
-	"os"
-	"reflect"
 	"testing"
 
 	"github.com/firehydrant/signals-migrator/pager"
 	"github.com/firehydrant/signals-migrator/store"
-	"github.com/gosimple/slug"
 )
 
-func pagerDutyHttpServer(t *testing.T) *httptest.Server {
-	h := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		filename, err := url.JoinPath("testdata", t.Name(), slug.Make(r.URL.Path)+".json")
-		if err != nil {
-			t.Fatalf("error joining path for expected response: %s", err)
-		}
-		if _, err := os.Stat(filename); err != nil {
-			t.Logf("file not found: %s", filename)
-			http.NotFound(w, r)
-			return
-		}
-		http.ServeFile(w, r, filename)
-	})
-	return httptest.NewServer(h)
-}
-
 func pagerDutyTestClient(t *testing.T, apiURL string) (context.Context, *pager.PagerDuty) {
+	t.Helper()
+
 	ctx := withTestDB(t)
 	pd := pager.NewPagerDutyWithURL("api-key-very-secret", apiURL)
 	return ctx, pd
 }
 
-func assertDeepEqual[T any](t *testing.T, got, want T) {
-	t.Helper()
-	if !reflect.DeepEqual(got, want) {
-		t.Fatalf("[FAIL]\n got: %+v\nwant: %+v", got, want)
-	}
-}
-
 func TestPagerDuty(t *testing.T) {
-	ts := pagerDutyHttpServer(t)
+	ts := pagerProviderHttpServer(t)
 	ctx, pd := pagerDutyTestClient(t, ts.URL)
 
 	t.Run("ListUsers", func(t *testing.T) {
 		u, err := pd.ListUsers(ctx)
 		if err != nil {
-			t.Fatalf("error loading schedules: %s", err)
+			t.Fatalf("error loading users: %s", err)
 		}
 		t.Logf("found %d users", len(u))
-
-		// Verify that the first user is as expected.
-		mika := u[0]
-		expected := &pager.User{
-			Email: "mika+eng@example.com",
-			Resource: pager.Resource{
-				ID:   "P5A1XH2",
-				Name: "Mika",
-			},
-		}
-		assertDeepEqual(t, mika, expected)
+		assertJSON(t, u)
 	})
 
 	t.Run("LoadSchedules", func(t *testing.T) {
@@ -80,20 +43,6 @@ func TestPagerDuty(t *testing.T) {
 			t.Fatalf("error loading schedules: %s", err)
 		}
 		t.Logf("found %d schedules", len(s))
-
-		// Verify that the first schedule is as expected.
-		first := s[0]
-		expected := store.ExtSchedule{
-			ID:            "P3D7DLW-PC1DX4O",
-			Name:          "Jen - primary - Layer 2",
-			Description:   "(Layer 2)",
-			Timezone:      "America/Los_Angeles",
-			Strategy:      "weekly",
-			ShiftDuration: "",
-			StartTime:     "",
-			HandoffTime:   "16:00:00",
-			HandoffDay:    "monday",
-		}
-		assertDeepEqual(t, first, expected)
+		assertJSON(t, s)
 	})
 }
